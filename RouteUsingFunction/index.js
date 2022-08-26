98c8d1b45d0a12f4de8f590dd4e31f0d54b0286f
Yuv Raj <yuvraj.srivastava6392@gmail.com>
2022-08-26 23:01:25 +0530

    added

--- RouteUsingFunction/index.js
+++ RouteUsingFunction/index.js
@@ -4,60 +4,59 @@ const httpStatus = require('http-status-codes');
 
 const port = 3000;
 
-// const getViewUrl = (url) => {
-//     if(url === '/'){
-//         return `views/index.html`;
-//     }
-//     return `views/${url}.html`;
-// }
-
-// const app = http.createServer((req, res) => {
-//         fs.readFile(getViewUrl(req.url), (error, data) => {
-//             if(error){
-//                 res.writeHead(httpStatus.NOT_FOUND, {'Content-Type': 'text/html'});
-//                 res.write("<h1> Not Found </h1>");
-//                 res.end();
-//                 return;
-//             }
-//             res.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
-//             res.write(data);
-//             res.end();
-//         });
-// });
-
-app.listen(port);
-
-module.exports = {app, getViewUrl};
-
-
-
-
-
-
-
-
-
-const getUrl=(url)=>{
-    if(url==='/')
-    {
-        return `views/index.html`
+const getViewUrl = (url) => {
+    if(url === '/'){
+        return `views/index.html`;
     }
-    else
-    {
-        return `views/${url}.html`;
-    }
+    return `views/${url}.html`;
 }
-app=app.createServer((req,res)=>{
-    fs.readFile(getUrl(req.url),(err,data)=>{
-        if(err)
-        {
-            res.write("404");
-            res.end();
-        }
-        else
-        {
+
+const app = http.createServer((req, res) => {
+        fs.readFile(getViewUrl(req.url), (error, data) => {
+            if(error){
+                res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {'Content-Type': 'text/html'});
+                res.write("<h1> Not Found </h1>");
+                res.end();
+                return;
+            }
+            res.writeHead(httpStatus.OK, {'Content-Type': 'text/html'});
             res.write(data);
             res.end();
-        }
-    })
-})
+        });
+});
+
+app.listen(port);
+console.log('started!')
+
+
+
+
+
+
+
+
+
+// const getUrl=(url)=>{
+//     if(url==='/')
+//     {
+//         return `views/index.html`
+//     }
+//     else
+//     {
+//         return `views/${url}.html`;
+//     }
+// }
+// app=app.createServer((req,res)=>{
+//     fs.readFile(getUrl(req.url),(err,data)=>{
+//         if(err)
+//         {
+//             res.write("404");
+//             res.end();
+//         }
+//         else
+//         {
+//             res.write(data);
+//             res.end();
+//         }
+//     })
+// })
